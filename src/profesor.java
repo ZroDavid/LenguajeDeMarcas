9e26ee342c639bea4234a65710e7145900c5fe0e
vesprada <vesprada@A53PC06>
2021-03-16 19:15:04 +0100

    Implementado constructor

--- src/profesor.java
+++ src/profesor.java
@@ -4,4 +4,13 @@ public class profesor {
 	private String nombre;
 	private int dni;
 	private int tel;
+	
+	private profesor(String nombre, int dni, int tel) {
+		super();
+		this.nombre = nombre;
+		this.dni = dni;
+		this.tel = tel;
+	}
+	
+	
 }
