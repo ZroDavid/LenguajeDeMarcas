2cd2922136875d611e700a844b1a9c10d32f503e
vesprada <vesprada@A53PC06>
2021-03-16 19:22:09 +0100

    Getters and Setters

--- src/profesor.java
+++ src/profesor.java
@@ -11,6 +11,32 @@ public class profesor {
 		this.dni = dni;
 		this.tel = tel;
 	}
+
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getDni() {
+		return dni;
+	}
+
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+
+	public int getTel() {
+		return tel;
+	}
+
+	public void setTel(int tel) {
+		this.tel = tel;
+	}
 	
 	
 }
